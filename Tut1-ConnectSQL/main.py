c3eeba0582e65902cdaf61e503d2ab0403b0b87d
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-08-23 21:36:09 +0700

    Initial commit

---
+++ Tut1-ConnectSQL/main.py
@@ -0,0 +1,50 @@
+from cgitb import reset
+import pyodbc
+import pandas as pd
+
+#driver: 
+#server name:
+#database name:
+#trusted_connection:  default yes
+
+# connect to sql
+connection = pyodbc.connect('Driver={SQL Server};'
+                            'Server=DESKTOP-IDBDCHG\SQLEXPRESS;'
+                            'Database=ConnectSQL;'
+                            'Trusted_Connection=yes;')
+cursor = connection.cursor()
+query = "select * from Person"
+
+
+
+# execute query
+
+def insert(person):
+    query = "insert into Person(Name,Address) values (N'{name}',N'{address}')".format(name = person[0],address = person[1])
+    return cursor.execute(query)
+
+
+
+person = ("Do thanhton ","Huwng yen")
+
+
+result = insert(person)
+
+
+if result:
+    print('Thanh cong')
+else:
+    print('That bai')
+
+
+
+
+# read sql from scratch
+cursor.execute(query)
+for i in cursor:
+    print(i[0])
+
+
+#using pandas
+df = pd.read_sql_query(query,connection)
+print(df)
